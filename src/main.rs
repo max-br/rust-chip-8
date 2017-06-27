912519c5533107cc7217c5554e647cd429f3cd7e
maxbr <maxbr@mi.fu-berlin.de>
2017-06-27 14:48:09 +0200

    fix semicolons

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 struct Chip8 {
-    ram: [u8; 4096];
-    stack: [u16; 16];
-    v: [u8; 16]; // registers
-    i: u16; // I register
-    timers: [u16; 2]; // Delay and Sound timers
-    pc: u16; // program counter
-    sp: u8; // stack pointer
-    buf: [bool; 64*32]; // display buffer
+    ram: [u8; 4096],
+    stack: [u16; 16],
+    v: [u8; 16], // registers
+    i: u16, // I register
+    timers: [u16; 2], // Delay and Sound timers
+    pc: u16, // program counter
+    sp: u8, // stack pointer
+    buf: [bool; 64*32], // display buffer
 }
 
 fn main() {
     println!("Hello, world!");
-
 }
